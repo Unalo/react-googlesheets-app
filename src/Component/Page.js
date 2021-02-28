842e6f326782d57f927e98ef02ead8b90d2c9fa6
unalo <unalomtshokotsha@gmail.com>
2021-02-28 22:04:27 +0200

    added state, changeHandler and onSubmitHandler

--- src/Component/Page.js
+++ src/Component/Page.js
@@ -1,31 +1,98 @@
-import React from "react";
+// import React from "react";
+import React, { Component } from 'react'
+
 import { Button, Form, Container, Header, Grid, Segment } from 'semantic-ui-react'
 
-const Page = (props)=> {
-  return(
+
+export default class Page extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+       name: '',
+       age: '',
+       salary: '',
+       hobby: ''
+    }
+  }
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  submitHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    const { name, age, salary, hobby } = this.state;    
+    return (
       <Container fluid className="container">
         <Header as='h2'>React Google Sheets!</Header>
-          <Form className="form">
-              <Form.Field>
-                <label>Name</label>
-              <input placeholder='Enter your name' />
-           </Form.Field>
-            <Form.Field>
-              <label>Age</label>
-              <input placeholder='Enter your age' />
+        <Form className="form" onSubmit={this.submitHandler}>
+          <Form.Field>
+            <label>Name</label>
+            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Age</label>
+            <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={this.changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Salary</label>
-            <input placeholder='Enter your salary' />
+            <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={this.changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Hobby</label>
-            <input placeholder='Enter your hobby' />
+            <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={this.changeHandler}/>
           </Form.Field>
           
           <Button color="blue" type='submit'>Submit</Button>
         </Form>
-    </Container>
-  )
+      </Container>
+    )
+  }
 }
-export default Page;
+
+
+// const Page = (props)=> {
+//   this.state = {
+//     name: '',
+//     age: '',
+//     salary: '',
+//     hobby: ''
+//  }
+//  changeHandler = (e) => {
+//   this.setState({[e.target.name] : e.target.value})
+// }
+
+//   return(
+//       <Container fluid className="container">
+//         <Header as='h2'>React Google Sheets!</Header>
+//           <Form className="form">
+//               <Form.Field>
+//                 <label>Name</label>
+//               <input placeholder='Enter your name' />
+//            </Form.Field>
+//             <Form.Field>
+//               <label>Age</label>
+//               <input placeholder='Enter your age' />
+//           </Form.Field>
+//           <Form.Field>
+//             <label>Salary</label>
+//             <input placeholder='Enter your salary' />
+//           </Form.Field>
+//           <Form.Field>
+//             <label>Hobby</label>
+//             <input placeholder='Enter your hobby' />
+//           </Form.Field>
+          
+//           <Button color="blue" type='submit'>Submit</Button>
+//         </Form>
+//     </Container>
+//   )
+// }
+// export default Page;
