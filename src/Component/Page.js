bd70eb20a980e3369fb9113774c796dc4710ee20
unalo <unalomtshokotsha@gmail.com>
2021-03-11 11:50:35 +0200

    added submit message, also validate the form fields

--- src/Component/Page.js
+++ src/Component/Page.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
-import { Button, Form, Container, Header, Grid, Message } from 'semantic-ui-react'
+import { Button, Form, Container, Header, Message } from 'semantic-ui-react'
 import axios from "axios";
 
 export default class Page extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
        name: '',
        age: '',
        salary: '',
-       hobby: ''
+       hobby: '',
+       errrors : {
+
+       },
+       submittedMessage : false
     }
+
+    // this.readyToSubmit = this.readyToSubmit.bind(this);
+
   }
 
   // keep track of our state
@@ -19,6 +25,17 @@ export default class Page extends Component {
     this.setState({
       [e.target.name] : e.target.value
     })
+  }
+
+  readyToSubmit = () => {
+      const ready = this.state.name 
+        && this.state.age
+        && this.state.salary
+        && this.state.hobby
+      if (!ready) {
+        return false;
+      }
+      return true;
   }
 
   submitHandler = e => {
@@ -34,8 +51,16 @@ export default class Page extends Component {
         name :'',
         age: '',
         salary: '',
-        hobby: ''
-      })
+        hobby: '',
+        submittedMessage : true
+      });
+
+
+      setTimeout(() => {
+        this.setState({
+          submittedMessage : false
+        })
+      }, 1000)
   }
 
   render() {
@@ -44,30 +69,38 @@ export default class Page extends Component {
     return (
       <Container fluid className="container">
         <Header as='h2'>React Google Sheets!</Header>
-        <Form success className="form" onSubmit={this.submitHandler}>
-          <Form.Field>
-            <label>Name</label>
-            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Age</label>
-            <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={this.changeHandler}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Salary</label>
-            <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={this.changeHandler}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Hobby</label>
-            <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={this.changeHandler}/>
-          </Form.Field>
-          <Message 
+        
+        {
+          !this.state.submittedMessage && <Form success className="form" onSubmit={this.submitHandler}>
+            <Form.Field>
+              <label>Name</label>
+              <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
+            </Form.Field>
+            <Form.Field>
+              <label>Age</label>
+              <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={this.changeHandler}/>
+            </Form.Field>
+            <Form.Field>
+              <label>Salary</label>
+              <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={this.changeHandler}/>
+            </Form.Field>
+            <Form.Field>
+              <label>Hobby</label>
+              <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={this.changeHandler}/>
+            </Form.Field>
+
+            <Button color="blue" type='submit' disabled={!this.readyToSubmit()} >Submit</Button>
+          </Form>
+        }
+
+
+        { this.state.submittedMessage && <Message 
           success
           header = "form Completed"
           content = "Data captureed"
-          />
-          <Button color="blue" type='submit'>Submit</Button>
-        </Form>
+          /> }
+
+
       </Container>
     )
   }
